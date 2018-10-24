386cbf2663ba9575237d23796fd98c3b9ddd437d
squarezeke <lucasbinswanger@gmail.com>
2018-10-24 00:38:18 -0500

    Updates

--- js/main.js
+++ js/main.js
@@ -6,64 +6,6 @@ const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
 // Initialize Firebase
-var config = {
-  apiKey: "AIzaSyCFmONmcYXo05dniZSP_W-GoaVkRaGZpTQ",
-  authDomain: "contactform-4eb14.firebaseapp.com",
-  databaseURL: "https://contactform-4eb14.firebaseio.com",
-  projectId: "contactform-4eb14",
-  storageBucket: "contactform-4eb14.appspot.com",
-  messagingSenderId: "761016789881"
-};
-firebase.initializeApp(config);
-
-// Refernce message collection
-
-var messageRef = firebase.database().ref("messages");
-
-// Listen for form submit
-document.getElementById("contactform").addEventListener("submit", submitForm);
-
-//Submit Form
-function submitForm(e) {
-  e.preventDefault();
-
-  // Get Values
-  var name = getInputVal("name");
-  var company = getInputVal("company");
-  var email = getInputVal("email");
-  var phone = getInputVal("phone");
-  var message = getInputVal("message");
-  //save message
-  saveMessage(name, company, email, phone, message);
-
-  // show alert
-  document.querySelector(".alert").style.display = "block";
-
-  // hide alert after 3 seconds
-  setTimeout(function() {
-    document.querySelector(".alert").style.display = "none";
-  }, 3000);
-
-  document.getElementById("contactform").reset();
-}
-
-// Function to get form values
-
-function getInputVal(id) {
-  return document.getElementById(id).value;
-}
-// Save message to firebase
-
-function saveMessage(name, company, email, phone, message) {
-  var newMessageRef = messageRef.push();
-  newMessageRef.set({
-    name: name,
-    company: company,
-    email: email,
-    phone: phone,
-    message: message
-  });
-}
 
 // Set Initial State of the Menu
 
